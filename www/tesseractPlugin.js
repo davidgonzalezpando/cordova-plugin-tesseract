20679d21a5e1b5208bd9605b53145f2a5ad42262
DavidGP <david.gonzalez.pando@gmail.com>
2022-06-29 13:03:35 +0200

    Update tesseractPlugin.js
    change installation

--- www/tesseractPlugin.js
+++ www/tesseractPlugin.js
@@ -1,9 +1,27 @@
-var TesseractPlugin = {
-    recognizeText: function (image, language, successCallback, errorCallback) {
-        cordova.exec(successCallback, errorCallback, "TesseractPlugin", "recognizeText", [language, image]);
-    },
-    loadLanguage: function (language, successCallback, errorCallback) {
-        cordova.exec(successCallback, errorCallback, "TesseractPlugin", "loadLanguage", [language]);
-    }
+// Empty constructor
+function TesseractPlugin() {}
+
+// The function that passes work along to native shells
+// Message is a string, duration may be 'long' or 'short'
+TesseractPlugin.prototype.recognizeText = function(language, image, successCallback, errorCallback) {
+  var options = {};
+  options.image = image;
+  options.language = language;
+  cordova.exec(successCallback, errorCallback, 'TesseractPlugin', 'recognizeText', [options]);
+}
+
+TesseractPlugin.prototype.echo = function(message, successCallback, errorCallback) {
+    var options = {};
+    options.message = message;
+    cordova.exec(successCallback, errorCallback, 'TesseractPlugin', 'echo', [options]);
+}
+
+// Installation constructor that binds TesseractPlugin to window
+TesseractPlugin.install = function() {
+  if (!window.plugins) {
+    window.plugins = {};
+  }
+  window.plugins.tesseractPlugin = new TesseractPlugin();
+  return window.plugins.tesseractPlugin;
 };
-module.exports = TesseractPlugin;
+cordova.addConstructor(TesseractPlugin.install);
