a59d09487c0c5e2b648c0c0698e4b6480cab3b9c
DavidGP <david.gonzalez.pando@gmail.com>
2022-06-29 17:00:39 +0200

    Update tesseractPlugin.js

--- www/tesseractPlugin.js
+++ www/tesseractPlugin.js
@@ -1,5 +1,6 @@
 var TesseractPlugin = {
     recognizeText: function (image, language, successCallback, errorCallback) {
+        alert('hello');
         cordova.exec(successCallback, errorCallback, "TesseractPlugin", "recognizeText", [language, image]);
     },
     loadLanguage: function (language, successCallback, errorCallback) {
